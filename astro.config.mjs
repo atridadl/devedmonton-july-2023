e0d575fe3b23f4e22b4b6279ac4f58c90931679d
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-19 00:35:35 -0600

    now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
@@ -7,21 +6,17 @@ import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000
-  },
   integrations: [preact(), react(), solidJs(), svelte(), vue(), tailwind()],
   output: "hybrid",
   compressHTML: true,
   vite: {
     ssr: {
-      noExternal: ["react-icons"]
-    }
-  }
+      noExternal: ["react-icons"],
+    },
+  },
 });
